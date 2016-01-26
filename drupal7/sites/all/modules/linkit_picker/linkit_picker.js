4b2335c5f4258366b6acfd9ccc2e7291d1a96bfe
Dave Lane <dave.lane@catalyst.net.nz>
2016-01-26 22:02:38 +1300

    major updates - modules, working theme, nearly ready to push

--- drupal7/sites/all/modules/linkit_picker/linkit_picker.js
+++ drupal7/sites/all/modules/linkit_picker/linkit_picker.js
@@ -2,23 +2,29 @@
 
 	Drupal.behaviors.linkit_picker_buttons = {
 		attach: function(context) {
-			$('.linkit_picker_button', context).click(function() {
-				var type = $(this).attr('id').replace('edit-','');
+			$('a.linkit-picker-button:not(.linkit-processed)').addClass('linkit-processed').click(function() {
+			  $('#linkit-picker-container a.linkit-picker-button').removeClass('selected');
+			  $(this).addClass('selected');
+				var viewname = $(this).data('viewname');
 				$('#linkit-picker-container .view-container').hide();
-				$('#linkit-picker-container .view-linkit-picker-' + type).parents('.view-container').toggle();
+				$('#linkit-picker-container .view-linkit-picker-' + viewname).toggle();
 				return false;
 			});
-			$('#linkit-picker-container .view-container:first').show();
+			$('a.linkit-picker-button:first', context).click();
 		}
 	}
-	
+
 	Drupal.behaviors.linkit_picker = {
 		attach: function(context) {
-			$('.view-display-id-default tr td', context).click(function() {
-				var type = $(this).parent().find('.views-field-linkit-picker');
-				$('.form-item-link input.form-text').val($.trim(type.text())).focus();
+			$('#linkit-picker-container .view-container tr td:not(.linkit-row-processed)').addClass('linkit-row-processed').click(function() {
+				var result = $(this).parent().find('.views-field-linkit-picker');
+				result = jQuery.parseJSON($.trim(result.text()));
+				Drupal.linkit.populateFields({
+          path: result.path
+        });
+        Drupal.settings.linkit.currentInstance.linkContent = result.title;
+        $('.linkit-path-element', context).focus();
 				$('html, body').animate({scrollTop:0}, 200);
-				$('#edit-browser-wrapper.collapsible legend a').click();
 			});
 		}
 	}
--- drupal7/sites/all/themes/cacophony2015/templates/page.tpl.php
+++ drupal7/sites/all/themes/cacophony2015/templates/page.tpl.php
@@ -125,9 +125,11 @@
     <?php print render($page['header']); ?>
   </header> <!-- /#page-header -->
 
-  <div class="container-fluid">
-    <?php print render($page['banner']); ?>
-  </div>
+  <?php if (!empty($page['banner'])): ?>
+    <div class="container-fluid">
+      <?php print render($page['banner']); ?>
+    </div>
+  <?php endif; ?>
 
   <div class="row">
 
@@ -141,6 +143,10 @@
       <?php if (!empty($tabs)): ?>
         <?php print render($tabs); ?>
       <?php endif; ?>
+      <?php print $messages; ?>
+      <?php if (!empty($page['help'])): ?>
+        <?php print render($page['help']); ?>
+      <?php endif; ?>
       <?php if (!empty($page['highlighted'])): ?>
         <div class="highlighted-wrapper"><div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div></div>
       <?php endif; ?>
@@ -151,10 +157,6 @@
         <h1 class="page-header"><?php print $title; ?></h1>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php if (!empty($page['help'])): ?>
-        <?php print render($page['help']); ?>
-      <?php endif; ?>
       <?php if (!empty($action_links)): ?>
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
